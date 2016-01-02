f20684fdbfa11d5433656607fdf093bda90ed461
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-01-02 17:17:17 +0100

    Add HTML import tests to verify the Wiki renderer

--- regtests/wiki-writers-tests.adb
+++ regtests/wiki-writers-tests.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  Render Tests - Unit tests for AWA Wiki rendering
---  Copyright (C) 2013 Stephane Carrez
+--  Copyright (C) 2013, 2016 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,10 +22,15 @@ with Ada.Characters.Conversions;
 
 with Util.Files;
 with Util.Measures;
+
+with Wiki.Render.Wiki;
+with Wiki.Filters.Html;
+with Wiki.Writers.Builders;
 with Wiki.Utils;
 package body Wiki.Writers.Tests is
 
    use Ada.Strings.Unbounded;
+   use type Wiki.Parsers.Wiki_Syntax_Type;
 
    --  ------------------------------
    --  Test rendering a wiki text in HTML or text.
@@ -43,7 +48,19 @@ package body Wiki.Writers.Tests is
       declare
          Time : Util.Measures.Stamp;
       begin
-         if T.Is_Html then
+         if T.Source = Wiki.Parsers.SYNTAX_HTML then
+            declare
+               Html_Filter : aliased Wiki.Filters.Html.Html_Filter_Type;
+               Writer      : aliased Wiki.Writers.Builders.Writer_Builder_Type;
+               Renderer    : aliased Wiki.Render.Wiki.Wiki_Renderer;
+            begin
+               Renderer.Set_Writer (Writer'Unchecked_Access, T.Format);
+               Html_Filter.Set_Document (Renderer'Unchecked_Access);
+               Wiki.Parsers.Parse (Html_Filter'Unchecked_Access, To_Wide (To_String (Content)),
+                                   Wiki.Parsers.SYNTAX_HTML);
+               Content := To_Unbounded_String (Writer.To_String);
+            end;
+         elsif T.Is_Html then
             Content := To_Unbounded_String
               (Utils.To_Html (To_Wide (To_String (Content)), T.Format));
          else
@@ -67,7 +84,9 @@ package body Wiki.Writers.Tests is
    overriding
    function Name (T : in Test) return Util.Tests.Message_String is
    begin
-      if T.Is_Html then
+      if T.Source = Wiki.Parsers.SYNTAX_HTML then
+         return Util.Tests.Format ("Test IMPORT " & To_String (T.Name));
+      elsif T.Is_Html then
          return Util.Tests.Format ("Test HTML " & To_String (T.Name));
       else
          return Util.Tests.Format ("Test TEXT " & To_String (T.Name));
@@ -85,72 +104,128 @@ package body Wiki.Writers.Tests is
 
    procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite) is
       use Ada.Directories;
-      function Create_Test (Name : in String; Is_Html : in Boolean) return Test_Case_Access;
+      function Create_Test (Name    : in String;
+                            Path    : in String;
+                            Format  : in Wiki.Parsers.Wiki_Syntax_Type;
+                            Prefix  : in String;
+                            Is_Html : in Boolean) return Test_Case_Access;
 
       Result_Dir  : constant String := "regtests/result";
-      Dir         : constant String := "regtests/files/wiki";
       Expect_Dir  : constant String := "regtests/expect";
-      Path        : constant String := Util.Tests.Get_Path (Dir);
       Expect_Path : constant String := Util.Tests.Get_Path (Expect_Dir);
       Result_Path : constant String := Util.Tests.Get_Test_Path (Result_Dir);
       Search      : Search_Type;
       Filter      : constant Filter_Type := (others => True);
       Ent         : Directory_Entry_Type;
 
-      function Create_Test (Name : in String; Is_Html : in Boolean) return Test_Case_Access is
-         Ext    : constant String := Ada.Directories.Extension (Name);
+      function Create_Test (Name    : in String;
+                            Path    : in String;
+                            Format  : in Wiki.Parsers.Wiki_Syntax_Type;
+                            Prefix  : in String;
+                            Is_Html : in Boolean) return Test_Case_Access is
          Tst    : Test_Case_Access;
       begin
          Tst := new Test;
          Tst.Is_Html := Is_Html;
          Tst.Name    := To_Unbounded_String (Name);
-         Tst.File    := To_Unbounded_String (Path & "/" & Name);
-         if Is_Html then
-            Tst.Expect  := To_Unbounded_String (Expect_Path & "/wiki-html/" & Name);
-            Tst.Result  := To_Unbounded_String (Result_Path & "/wiki-html/" & Name);
-         else
-            Tst.Expect  := To_Unbounded_String (Expect_Path & "/wiki-txt/" & Name);
-            Tst.Result  := To_Unbounded_String (Result_Path & "/wiki-txt/" & Name);
-         end if;
-         if Ext = "wiki" then
-            Tst.Format := Wiki.Parsers.SYNTAX_GOOGLE;
-         elsif Ext = "dotclear" then
-            Tst.Format := Wiki.Parsers.SYNTAX_DOTCLEAR;
-         elsif Ext = "creole" then
-            Tst.Format := Wiki.Parsers.SYNTAX_CREOLE;
-         elsif Ext = "phpbb" then
-            Tst.Format := Wiki.Parsers.SYNTAX_PHPBB;
-         elsif Ext = "mediawiki" then
-            Tst.Format := Wiki.Parsers.SYNTAX_MEDIA_WIKI;
-         else
-            Tst.Format := Wiki.Parsers.SYNTAX_MIX;
-         end if;
+         Tst.File    := To_Unbounded_String (Path);
+         Tst.Expect  := To_Unbounded_String (Expect_Path & Prefix & Name);
+         Tst.Result  := To_Unbounded_String (Result_Path & Prefix & Name);
+         Tst.Format  := Format;
+         Tst.Source  := Format;
          return Tst;
       end Create_Test;
 
+      procedure Add_Wiki_Tests is
+         Dir         : constant String := "regtests/files/wiki";
+         Path        : constant String := Util.Tests.Get_Path (Dir);
+      begin
+         if Kind (Path) /= Directory then
+            Ada.Text_IO.Put_Line ("Cannot read test directory: " & Path);
+         end if;
+
+         Start_Search (Search, Directory => Path, Pattern => "*.*", Filter => Filter);
+         while More_Entries (Search) loop
+            Get_Next_Entry (Search, Ent);
+            declare
+               Simple : constant String := Simple_Name (Ent);
+               Ext    : constant String := Ada.Directories.Extension (Simple);
+               Tst    : Test_Case_Access;
+               Format : Wiki.Parsers.Wiki_Syntax_Type;
+            begin
+               if Simple /= "." and then Simple /= ".."
+                 and then Simple /= ".svn" and then Simple (Simple'Last) /= '~'
+               then
+                  if Ext = "wiki" then
+                     Format := Wiki.Parsers.SYNTAX_GOOGLE;
+                  elsif Ext = "dotclear" then
+                     Format := Wiki.Parsers.SYNTAX_DOTCLEAR;
+                  elsif Ext = "creole" then
+                     Format := Wiki.Parsers.SYNTAX_CREOLE;
+                  elsif Ext = "phpbb" then
+                     Format := Wiki.Parsers.SYNTAX_PHPBB;
+                  elsif Ext = "mediawiki" then
+                     Format := Wiki.Parsers.SYNTAX_MEDIA_WIKI;
+                  else
+                     Format := Wiki.Parsers.SYNTAX_MIX;
+                  end if;
+
+                  Tst := Create_Test (Simple, Path & "/" & Simple, Format, "/wiki-html/", True);
+                  Suite.Add_Test (Tst.all'Access);
+
+                  Tst := Create_Test (Simple, Path & "/" & Simple, Format, "/wiki-txt/", False);
+                  Suite.Add_Test (Tst.all'Access);
+               end if;
+            end;
+         end loop;
+      end Add_Wiki_Tests;
+
+      procedure Add_Import_Tests is
+         Dir         : constant String := "regtests/files/html";
+         Path        : constant String := Util.Tests.Get_Path (Dir);
+      begin
+         if Kind (Path) /= Directory then
+            Ada.Text_IO.Put_Line ("Cannot read test directory: " & Path);
+         end if;
+
+         Start_Search (Search, Directory => Path, Pattern => "*.*", Filter => Filter);
+         while More_Entries (Search) loop
+            Get_Next_Entry (Search, Ent);
+            declare
+               Simple : constant String := Simple_Name (Ent);
+               Name   : constant String := Base_Name (Simple);
+               Tst    : Test_Case_Access;
+            begin
+               if Simple /= "." and then Simple /= ".."
+                 and then Simple /= ".svn" and then Simple (Simple'Last) /= '~'
+               then
+                  for Syntax in Wiki.Parsers.Wiki_Syntax_Type'Range loop
+                     case Syntax is
+                        when Wiki.Parsers.SYNTAX_CREOLE =>
+                           Tst := Create_Test (Name & ".creole", Path & "/" & Simple,
+                                               Syntax, "/wiki-import/", True);
+
+                        when Wiki.Parsers.SYNTAX_MEDIA_WIKI =>
+                           Tst := Create_Test (Name & ".mediawiki", Path & "/" & Simple,
+                                               Syntax, "/wiki-import/", True);
+
+                        when others =>
+                           Tst := null;
+
+                     end case;
+                     if Tst /= null then
+                        Tst.Source := Wiki.Parsers.SYNTAX_HTML;
+                        Suite.Add_Test (Tst.all'Access);
+                     end if;
+                  end loop;
+               end if;
+            end;
+         end loop;
+      end Add_Import_Tests;
+
    begin
-      if Kind (Path) /= Directory then
-         Ada.Text_IO.Put_Line ("Cannot read test directory: " & Path);
-      end if;
-
-      Start_Search (Search, Directory => Path, Pattern => "*.*", Filter => Filter);
-      while More_Entries (Search) loop
-         Get_Next_Entry (Search, Ent);
-         declare
-            Simple : constant String := Simple_Name (Ent);
-            Tst    : Test_Case_Access;
-         begin
-            if Simple /= "." and then Simple /= ".."
-              and then Simple /= ".svn" and then Simple (Simple'Last) /= '~'
-            then
-               Tst := Create_Test (Simple, True);
-               Suite.Add_Test (Tst.all'Access);
-
-               Tst := Create_Test (Simple, False);
-               Suite.Add_Test (Tst.all'Access);
-            end if;
-         end;
-      end loop;
+      Add_Wiki_Tests;
+      Add_Import_Tests;
    end Add_Tests;
 
 end Wiki.Writers.Tests;
