06ab3a5a40e9247844ccae06fa9b678ce01c1a44
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-02-07 09:56:57 +0100

    - Remove Html_Tag_Type to use the Wiki.Nodes.Html_Tag_Type - Remove the Add_Text, Start_Element, End_Element old procedures

--- src/wiki-filters-html.ads
+++ src/wiki-filters-html.ads
@@ -40,77 +40,6 @@ private with Ada.Containers.Vectors;
 --
 package Wiki.Filters.Html is
 
-   --  The possible HTML tags as described in HTML5 specification.
-   type Html_Tag_Type is
-     (
-      --  Section 4.1 The root element
-      HTML_TAG,
-
-      --  Section 4.2 Document metadata
-      HEAD_TAG, TITLE_TAG, BASE_TAG, LINK_TAG, META_TAG, STYLE_TAG,
-
-      --  Section 4.3 Sections
-      BODY_TAG, ARTICLE_TAG, SECTION_TAG, NAV_TAG, ASIDE_TAG,
-      H1_TAG, H2_TAG, H3_TAG, H4_TAG, H5_TAG, H6_TAG,
-      HEADER_TAG, FOOTER_TAG,
-      ADDRESS_TAG,
-
-      --  Section 4.4 Grouping content
-      P_TAG, HR_TAG, PRE_TAG, BLOCKQUOTE_TAG,
-      OL_TAG, UL_TAG, LI_TAG,
-      DL_TAG, DT_TAG, DD_TAG,
-      FIGURE_TAG, FIGCAPTION_TAG,
-      DIV_TAG, MAIN_TAG,
-
-      --  Section 4.5 Text-level semantics
-      A_TAG, EM_TAG, STRONG_TAG, SMALL_TAG,
-      S_TAG, CITE_TAG, Q_TAG, DFN_TAG, ABBR_TAG,
-      DATA_TAG, TIME_TAG, CODE_TAG, VAR_TAG, SAMP_TAG,
-      KBD_TAG, SUB_TAG, SUP_TAG,
-      I_TAG, B_TAG, U_TAG,
-      MARK_TAG, RUBY_TAG, RB_TAG, RT_TAG, RTC_TAG,
-      RP_TAG, BDI_TAG, BDO_TAG, SPAN_TAG,
-      BR_TAG, WBR_TAG,
-
-      --  Section 4.6 Edits
-      INS_TAG, DEL_TAG,
-
-      --  Section 4.7 Embedded content
-      IMG_TAG,
-      IFRAME_TAG,
-      EMBED_TAG,
-      OBJECT_TAG,
-      PARAM_TAG,
-      VIDEO_TAG,
-      AUDIO_TAG,
-      SOURCE_TAG,
-      TRACK_TAG,
-      MAP_TAG,
-      AREA_TAG,
-
-      --  Section 4.9 Tabular data
-      TABLE_TAG, CAPTION_TAG, COLGROUP_TAG, COL_TAG,
-      TBODY_TAG, THEAD_TAG, TFOOT_TAG,
-      TR_TAG, TD_TAG, TH_TAG,
-
-      --  Section 4.10 Forms
-      FORM_TAG, LABEL_TAG, INPUT_TAG,
-      BUTTON_TAG, SELECT_TAG, DATALIST_TAG, OPTGROUP_TAG,
-      OPTION_TAG, TEXTAREA_TAG, KEYGEN_TAG, OUTPUT_TAG,
-      PROGRESS_TAG, METER_TAG, FIELDSET_TAG, LEGEND_TAG,
-
-      --  Section 4.11 Scripting
-      SCRIPT_TAG, NOSCRIPT_TAG,
-      TEMPLATE_TAG, CANVAS_TAG,
-
-      --  Unknown tags
-      UNKNOWN_TAG
-     );
-
-   --  Find the tag from the tag name.
-   function Find_Tag (Name : in Wide_Wide_String) return Html_Tag_Type;
-   function Find_Tag (Name : in Unbounded_Wide_Wide_String) return Html_Tag_Type;
-
    --  ------------------------------
    --  Filter type
    --  ------------------------------
@@ -149,12 +78,6 @@ package Wiki.Filters.Html is
                        Document : in out Wiki.Nodes.Document;
                         Tag     : in Wiki.Nodes.Html_Tag_Type);
 
-   --  Add a text block with the given format.
-   overriding
-   procedure Add_Text (Document : in out Html_Filter_Type;
-                       Text     : in Unbounded_Wide_Wide_String;
-                       Format   : in Wiki.Documents.Format_Map);
-
    --  Add a link.
    overriding
    procedure Add_Link (Document : in out Html_Filter_Type;
@@ -170,15 +93,6 @@ package Wiki.Filters.Html is
                         Alt         : in Unbounded_Wide_Wide_String;
                         Position    : in Unbounded_Wide_Wide_String;
                         Description : in Unbounded_Wide_Wide_String);
-
-   overriding
-   procedure Start_Element (Document   : in out Html_Filter_Type;
-                            Name       : in Unbounded_Wide_Wide_String;
-                            Attributes : in Wiki.Attributes.Attribute_List_Type);
-
-   overriding
-   procedure End_Element (Document : in out Html_Filter_Type;
-                          Name     : in Unbounded_Wide_Wide_String);
 
    --  Finish the document after complete wiki text has been parsed.
    overriding
@@ -205,6 +119,8 @@ package Wiki.Filters.Html is
    procedure Flush_Stack (Document : in out Html_Filter_Type);
 
 private
+
+   use Wiki.Nodes;
 
    type Tag_Boolean_Array is array (Html_Tag_Type) of Boolean;
 
