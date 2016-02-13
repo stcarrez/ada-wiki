f9be9b768ed61f6639a6da737f73281ec6c6c353
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-02-13 22:49:51 +0100

    Use the Document type

--- src/wiki-plugins.ads
+++ src/wiki-plugins.ads
@@ -17,7 +17,7 @@
 -----------------------------------------------------------------------
 
 with Wiki.Attributes;
-with Wiki.Documents;
+with Wiki.Nodes;
 
 --  == Plugins ==
 --  The <b>Wiki.Plugins</b> package defines the plugin interface that is used by the wiki
@@ -32,7 +32,7 @@ package Wiki.Plugins is
 
    --  Expand the plugin configured with the parameters for the document.
    procedure Expand (Plugin   : in out Wiki_Plugin;
-                     Document : in out Wiki.Documents.Document_Reader'Class;
+                     Document : in out Wiki.Nodes.Document;
                      Params   : in out Wiki.Attributes.Attribute_List_Type) is abstract;
 
 end Wiki.Plugins;
