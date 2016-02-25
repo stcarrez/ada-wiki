9513566956cf2c2e5d1bd02e889e50793f6b599c
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-02-25 23:43:06 +0100

    Use the Wiki.Strings.WString type

--- src/wiki-utils.ads
+++ src/wiki-utils.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  wiki-utils -- Wiki utility operations
---  Copyright (C) 2015 Stephane Carrez
+--  Copyright (C) 2015, 2016 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,16 +15,16 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+with Wiki.Strings;
 package Wiki.Utils is
 
    --  Render the wiki text according to the wiki syntax in HTML into a string.
-   function To_Html (Text   : in Wide_Wide_String;
+   function To_Html (Text   : in Wiki.Strings.WString;
                      Syntax : in Wiki.Wiki_Syntax) return String;
 
    --  Render the wiki text according to the wiki syntax in text into a string.
    --  Wiki formatting and decoration are removed.
-   function To_Text (Text   : in Wide_Wide_String;
+   function To_Text (Text   : in Wiki.Strings.WString;
                      Syntax : in Wiki.Wiki_Syntax) return String;
 
 end Wiki.Utils;
