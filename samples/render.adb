2578afda1432645b4e2abc450237dc2e35d1b3b1
Stephane Carrez <stcarrez@bouyguestelecom.fr>
2016-02-28 23:13:39 +0100

    Use the Text_IO streams to read and write the result Add a -s <style> option to emit a HTML head with CSS style

--- samples/render.adb
+++ samples/render.adb
@@ -18,40 +18,81 @@
 with Ada.Text_IO;
 with Ada.IO_Exceptions;
 with Ada.Strings.Unbounded;
-with Ada.Characters.Conversions;
 
 with GNAT.Command_Line;
 
-with Util.Files;
-
-with Wiki.Utils;
+with Wiki.Documents;
+with Wiki.Parsers;
+with Wiki.Filters.TOC;
+with Wiki.Filters.Html;
+with Wiki.Render.Html;
+with Wiki.Render.Text;
+with Wiki.Streams.Text_IO;
+with Wiki.Streams.Html.Text_IO;
 
 procedure Render is
 
    use GNAT.Command_Line;
    use Ada.Strings.Unbounded;
-   use Ada.Characters.Conversions;
 
    procedure Usage;
+   procedure Render_Html (Doc   : in out Wiki.Documents.Document;
+                          Style : in Unbounded_String);
+   procedure Render_Text (Doc : in out Wiki.Documents.Document);
 
    procedure Usage is
    begin
       Ada.Text_IO.Put_Line ("Render a wiki text file into HTML (default) or text");
-      Ada.Text_IO.Put_Line ("Usage: render [-t] [-m] [-M] [-d] [-c] {wiki-file}");
-      Ada.Text_IO.Put_Line ("  -t      Render to text only");
-      Ada.Text_IO.Put_Line ("  -m      Render a Markdown wiki content");
-      Ada.Text_IO.Put_Line ("  -M      Render a Mediawiki wiki content");
-      Ada.Text_IO.Put_Line ("  -d      Render a Dotclear wiki content");
-      Ada.Text_IO.Put_Line ("  -g      Render a Google wiki content");
-      Ada.Text_IO.Put_Line ("  -c      Render a Creole wiki content");
+      Ada.Text_IO.Put_Line ("Usage: render [-t] [-m] [-M] [-d] [-c] [-s style] {wiki-file}");
+      Ada.Text_IO.Put_Line ("  -t        Render to text only");
+      Ada.Text_IO.Put_Line ("  -m        Render a Markdown wiki content");
+      Ada.Text_IO.Put_Line ("  -M        Render a Mediawiki wiki content");
+      Ada.Text_IO.Put_Line ("  -d        Render a Dotclear wiki content");
+      Ada.Text_IO.Put_Line ("  -g        Render a Google wiki content");
+      Ada.Text_IO.Put_Line ("  -c        Render a Creole wiki content");
+      Ada.Text_IO.Put_Line ("  -s style  Use the CSS style file");
    end Usage;
+
+   procedure Render_Html (Doc   : in out Wiki.Documents.Document;
+                          Style : in Unbounded_String) is
+      Output   : aliased Wiki.Streams.Html.Text_IO.Html_File_Output_Stream;
+      Renderer : aliased Wiki.Render.Html.Html_Renderer;
+   begin
+      if Length (Style) > 0 then
+         Output.Start_Element ("html");
+         Output.Start_Element ("head");
+         Output.Start_Element ("link");
+         Output.Write_Attribute ("type", "text/css");
+         Output.Write_Attribute ("rel", "stylesheet");
+         Output.Write_Attribute ("href", To_String (Style));
+         Output.End_Element ("link");
+         Output.End_Element ("head");
+         Output.Start_Element ("body");
+      end if;
+      Renderer.Set_Output_Stream (Output'Unchecked_Access);
+      Renderer.Set_Render_TOC (True);
+      Renderer.Render (Doc);
+      if Length (Style) > 0 then
+         Output.End_Element ("body");
+         Output.End_Element ("html");
+      end if;
+   end Render_Html;
+
+   procedure Render_Text (Doc : in out Wiki.Documents.Document) is
+      Output   : aliased Wiki.Streams.Text_IO.File_Output_Stream;
+      Renderer : aliased Wiki.Render.Text.Text_Renderer;
+   begin
+      Renderer.Set_Output_Stream (Output'Unchecked_Access);
+      Renderer.Render (Doc);
+   end Render_Text;
 
    Count     : Natural := 0;
    Html_Mode : Boolean := True;
    Syntax    : Wiki.Wiki_Syntax := Wiki.SYNTAX_MARKDOWN;
+   Style     : Unbounded_String;
 begin
    loop
-      case Getopt ("m M d c g t") is
+      case Getopt ("m M d c g t s:") is
          when 'm' =>
             Syntax := Wiki.SYNTAX_MARKDOWN;
 
@@ -70,6 +111,9 @@ begin
          when 't' =>
             Html_Mode := False;
 
+         when 's' =>
+            Style := To_Unbounded_String (Parameter);
+
          when others =>
             exit;
       end case;
@@ -77,8 +121,12 @@ begin
 
    loop
       declare
-         Name : constant String := GNAT.Command_Line.Get_Argument;
-         Data : Unbounded_String;
+         Name     : constant String := GNAT.Command_Line.Get_Argument;
+         Input    : aliased Wiki.Streams.Text_IO.File_Input_Stream;
+         Filter   : aliased Wiki.Filters.Html.Html_Filter_Type;
+         TOC      : aliased Wiki.Filters.TOC.TOC_Filter;
+         Doc      : Wiki.Documents.Document;
+         Engine   : Wiki.Parsers.Parser;
       begin
          if Name = "" then
             if Count = 0 then
@@ -87,14 +135,21 @@ begin
             return;
          end if;
          Count := Count + 1;
-         Util.Files.Read_File (Name, Data);
+
+         --  Open the file and parse it (assume UTF-8).
+         Input.Open (Name, "WCEM=8");
+         Engine.Add_Filter (TOC'Unchecked_Access);
+         Engine.Add_Filter (Filter'Unchecked_Access);
+         Engine.Set_Syntax (Syntax);
+         Engine.Parse (Input'Unchecked_Access, Doc);
+
+         --  Render the document in text or HTML.
          if Html_Mode then
-            Ada.Text_IO.Put_Line
-              (Wiki.Utils.To_Html (To_Wide_Wide_String (To_String (Data)), Syntax));
+            Render_Html (Doc, Style);
          else
-            Ada.Text_IO.Put_Line
-              (Wiki.Utils.To_Text (To_Wide_Wide_String (To_String (Data)), Syntax));
+            Render_Text (Doc);
          end if;
+
       exception
          when Ada.IO_Exceptions.Name_Error =>
             Ada.Text_IO.Put_Line ("Cannot read file '" & Name & "'");
