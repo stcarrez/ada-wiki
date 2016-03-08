e3119d369fdbaeb7275dcd11971cba1216d655e5
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-03-08 23:15:26 +0100

    Add some documentation

--- src/wiki.ads
+++ src/wiki.ads
@@ -24,6 +24,28 @@
 --  * The Wiki text is parsed according to its syntax to produce a Wiki Document instance.
 --  * The Wiki document is then rendered by a renderer to produce the final HTML, text.
 --
+--  [images/ada-wiki.png]
+--
+--  The Ada Wiki engine is organized in several packages:
+--
+--  * The Wiki stream packages define the interface, types and operations for the Wiki
+--  engine to read the Wiki or HTML content and for the Wiki renderer to generate the
+--  HTML or text outputs.
+--  * The Wiki parser is responsible for parsing HTML or Wiki content according to a
+--  selected Wiki syntax.  It builds the final Wiki document through filters and plugins.
+--  * The Wiki filters provides a simple filter framework that allows to plug specific
+--  filters when a Wiki document is parsed and processed.  Filters are used for the
+--  table of content generation, for the HTML filtering, to collect words or links
+--  and so on.
+--  * The Wiki plugins defines the plugin interface that is used by the Wiki engine
+--  to provide pluggable extensions in the Wiki.  Plugins are used for the Wiki template
+--  support, to hide some Wiki text content when it is rendered or to interact with
+--  other systems.
+--  * The Wiki documents and attributes are used for the representation of the Wiki
+--  document after the Wiki content is parsed.
+--  * The Wiki renderers are the last packages which are used for the rendering of the
+--  Wiki document to produce the final HTML or text.
+--
 --  @include wiki-documents.ads
 --  @include wiki-attributes.ads
 --  @include wiki-parsers.ads
