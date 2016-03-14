f73ebf25f0ae9b182b70eff4d42a4176529c8bd2
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-03-14 22:36:03 +0100

    Implement the Add_Text procedure to identify the __TOC__ keywords and emit a N_TOC_DISPLAY node in the document

--- src/wiki-filters-toc.adb
+++ src/wiki-filters-toc.adb
@@ -15,8 +15,35 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+with Ada.Strings.Wide_Wide_Fixed;
 with Wiki.Nodes;
 package body Wiki.Filters.TOC is
+
+   --  ------------------------------
+   --  Add a text content with the given format to the document.
+   --  ------------------------------
+   overriding
+   procedure Add_Text (Filter    : in out TOC_Filter;
+                       Document  : in out Wiki.Documents.Document;
+                       Text      : in Wiki.Strings.WString;
+                       Format    : in Wiki.Format_Map) is
+      use Ada.Strings.Wide_Wide_Fixed;
+
+      First : Natural := Text'First;
+      Pos   : Natural;
+   begin
+      while First <= Text'Last loop
+         Pos := Index (Text (First .. Text'Last), "__TOC__");
+         if Pos > 0 then
+            Filter_Type (Filter).Add_Text (Document, Text (First .. Pos - 1), Format);
+            Filter.Add_Node (Document, Wiki.Nodes.N_TOC_DISPLAY);
+            First := Pos + 7;
+         else
+            Filter_Type (Filter).Add_Text (Document, Text (First .. Text'Last), Format);
+            exit;
+         end if;
+      end loop;
+   end Add_Text;
 
    --  ------------------------------
    --  Add a section header with the given level in the document.
