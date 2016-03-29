dff287f7fc6529801b322a620d00beb77aed2257
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-03-29 21:52:00 +0200

    Add some documentation

--- src/wiki-filters-collectors.ads
+++ src/wiki-filters-collectors.ads
@@ -18,11 +18,24 @@
 with Ada.Containers.Indefinite_Ordered_Maps;
 
 --  === Collector Filters ===
---  The <tt>Wiki.Filters.Collectors</tt> package defines two filters that can be used to
---  collect words or links contained in a Wiki document.  The collector filters are inserted
---  in the filter chain and they collect the data as the Wiki text is parsed.  After the
---  parsing, the collector filters have collected either the words or the links and they
---  can be queried by using the <tt>Find</tt> or <tt>Iterate</tt> operations.
+--  The <tt>Wiki.Filters.Collectors</tt> package defines three filters that can be used to
+--  collect words, links or images contained in a Wiki document.  The collector filters are
+--  inserted in the filter chain and they collect the data as the Wiki text is parsed.
+--  After the parsing, the collector filters have collected either the words or the links
+--  and they can be queried by using the <tt>Find</tt> or <tt>Iterate</tt> operations.
+--
+--  The filter is inserted in the filter chain before parsing the Wiki document.
+--
+--    Words : aliased Wiki.Filters.Collectors.Word_Collector_Type;
+--    ...
+--    Engine.Add_Filter (Words'Unchecked_Access);
+--
+--  Once the document is parsed, the collector filters contains the data that was collected.
+--  The <tt>Iterate</tt> procedure can be used to have a procedure called for each value
+--  collected by the filter.
+--
+--    Words.Iterate (Print'Access);
+--
 package Wiki.Filters.Collectors is
 
    pragma Preelaborate;
