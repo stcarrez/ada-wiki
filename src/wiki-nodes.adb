a81846ab1effd1fd676f755ac2a766f8b02cb698
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-08-15 20:42:50 +0200

    Refactor Node_List and references to use Ada Util general references

--- src/wiki-nodes.adb
+++ src/wiki-nodes.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  wiki-nodes -- Wiki Document Internal representation
---  Copyright (C) 2016 Stephane Carrez
+--  Copyright (C) 2016, 2019 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -51,7 +51,6 @@ package body Wiki.Nodes is
    --  ------------------------------
    --  Finalize the node list to release the allocated memory.
    --  ------------------------------
-   overriding
    procedure Finalize (List : in out Node_List) is
       procedure Free is
         new Ada.Unchecked_Deallocation (Node_List_Block, Node_List_Block_Access);
@@ -92,67 +91,5 @@ package body Wiki.Nodes is
       Release (List.First.Next);
       Free_Block (List.First);
    end Finalize;
-
-   --  ------------------------------
-   --  Iterate over the nodes of the list and call the <tt>Process</tt> procedure with
-   --  each node instance.
-   --  ------------------------------
-   procedure Iterate (List    : in Node_List_Access;
-                      Process : not null access procedure (Node : in Node_Type)) is
-      Block : Node_List_Block_Access := List.First'Access;
-   begin
-      loop
-         for I in 1 .. Block.Last loop
-            Process (Block.List (I).all);
-         end loop;
-         Block := Block.Next;
-         exit when Block = null;
-      end loop;
-   end Iterate;
-
-   --  ------------------------------
-   --  Append a node to the node list.
-   --  ------------------------------
-   procedure Append (Into : in out Node_List_Ref;
-                     Node : in Node_Type_Access) is
-   begin
-      if Into.Is_Null then
-         Node_List_Refs.Ref (Into) := Node_List_Refs.Create;
-         Into.Value.Current := Into.Value.First'Access;
-      end if;
-      Append (Into.Value.all, Node);
-   end Append;
-
-   --  ------------------------------
-   --  Iterate over the nodes of the list and call the <tt>Process</tt> procedure with
-   --  each node instance.
-   --  ------------------------------
-   procedure Iterate (List    : in Node_List_Ref;
-                      Process : not null access procedure (Node : in Node_Type)) is
-   begin
-      if not List.Is_Null then
-         Iterate (List.Value, Process);
-      end if;
-   end Iterate;
-
-   --  ------------------------------
-   --  Returns True if the list reference is empty.
-   --  ------------------------------
-   function Is_Empty (List : in Node_List_Ref) return Boolean is
-   begin
-      return List.Is_Null;
-   end Is_Empty;
-
-   --  ------------------------------
-   --  Get the number of nodes in the list.
-   --  ------------------------------
-   function Length (List : in Node_List_Ref) return Natural is
-   begin
-      if List.Is_Null then
-         return 0;
-      else
-         return List.Value.Length;
-      end if;
-   end Length;
 
 end Wiki.Nodes;
