4b19c10e4ffd8fd775eb3a47a46889013d0e686f
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-09-05 09:20:07 +0200

    Refactor the implementation to instantiate the generic Html.Stream package

--- src/wiki-streams-html-text_io.ads
+++ src/wiki-streams-html-text_io.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  wiki-streams-html-text_io -- Wiki HTML output stream on Ada Text_IO
---  Copyright (C) 2016 Stephane Carrez
+--  Copyright (C) 2016, 2020 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,60 +15,8 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-with Wiki.Strings;
 with Wiki.Streams.Text_IO;
+with Wiki.Streams.Html.Stream;
 
---  === HTML Output Stream ===
---  The <tt>Wiki.Writers</tt> package defines the interfaces used by the renderer to write
---  their outputs.
---
---  The <tt>Input_Stream</tt> interface defines the interface that must be implemented to
---  read the source Wiki content.  The <tt>Read</tt> procedure is called by the parser
---  repeatedly while scanning the Wiki content.
 package Wiki.Streams.Html.Text_IO is
-
-   type Html_File_Output_Stream is limited new Wiki.Streams.Text_IO.File_Output_Stream
-     and Html.Html_Output_Stream with private;
-
-   --  Write an XML attribute within an XML element.
-   --  The attribute value is escaped according to the XML escape rules.
-   overriding
-   procedure Write_Wide_Attribute (Stream  : in out Html_File_Output_Stream;
-                                   Name    : in String;
-                                   Content : in Wiki.Strings.UString);
-
-   --  Write an XML attribute within an XML element.
-   --  The attribute value is escaped according to the XML escape rules.
-   overriding
-   procedure Write_Wide_Attribute (Stream  : in out Html_File_Output_Stream;
-                                   Name    : in String;
-                                   Content : in Wide_Wide_String);
-
-   --  Start an XML element with the given name.
-   overriding
-   procedure Start_Element (Stream : in out Html_File_Output_Stream;
-                            Name   : in String);
-
-   --  Closes an XML element of the given name.
-   overriding
-   procedure End_Element (Stream : in out Html_File_Output_Stream;
-                          Name   : in String);
-
-   --  Write a text escaping any character as necessary.
-   overriding
-   procedure Write_Wide_Text (Stream  : in out Html_File_Output_Stream;
-                              Content : in Wiki.Strings.WString);
-
-   --  Write the string to the stream.
-   procedure Write_String (Stream  : in out Html_File_Output_Stream'Class;
-                           Content : in String);
-
-private
-
-   type Html_File_Output_Stream is limited new Wiki.Streams.Text_IO.File_Output_Stream
-     and Html.Html_Output_Stream with record
-      --  Whether an XML element must be closed (that is a '>' is necessary)
-      Close_Start : Boolean := False;
-   end record;
-
-end Wiki.Streams.Html.Text_IO;
+   new Wiki.Streams.Html.Stream (Wiki.Streams.Text_IO.File_Output_Stream);
