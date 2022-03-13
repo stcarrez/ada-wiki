e934ad99af342c778fd12f2302fbc4565b327fa4
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-03-13 19:03:26 +0100

    Implement the Parse_Image, Parse_Bold_Or_List and Parse_Link

--- src/wiki-parsers-textile.adb
+++ src/wiki-parsers-textile.adb
@@ -120,15 +120,61 @@ package body Wiki.Parsers.Textile is
       Clear (P.Text);
    end Parse_Header;
 
+   --  ------------------------------
    --  Parse a textile image.
    --  Example:
    --    !image-link!
    --    !image-link(title)!
-   --    !image-path|:http-link
+   --    !image-path!:http-link
+   --  ------------------------------
    procedure Parse_Image (P     : in out Parser;
                           Token : in Wiki.Strings.WChar) is
+      Pos       : Natural := P.Line_Pos + 1;
+      Last      : Natural;
+      C         : Wiki.Strings.WChar;
    begin
-      null;
+      Last := Wiki.Strings.Index (P.Line, '!', Pos);
+      if Last = 0 then
+         Append (P.Text, Token);
+         return;
+      end if;
+
+      declare
+         Title : Wiki.Strings.UString;
+         Link  : Wiki.Strings.UString;
+         Title_Pos : Natural;
+         Last_Pos  : Natural;
+      begin
+         Title_Pos := Wiki.Strings.Index (P.Line, '(', Pos);
+
+         if Title_Pos = 0 then
+            Last_Pos := Last;
+         else
+            Last_Pos := Title_Pos;
+         end if;
+
+         while Pos < Last_Pos loop
+            Wiki.Strings.Append (Link, Wiki.Strings.Element (P.Line, Pos));
+            Pos := Pos + 1;
+         end loop;
+
+         if Title_Pos > 0 then
+            Title_Pos := Title_Pos + 1;
+            while Title_Pos < Last - 1 loop
+               Wiki.Strings.Append (Title, Wiki.Strings.Element (P.Line, Title_Pos));
+               Title_Pos := Title_Pos + 1;
+            end loop;
+         end if;
+
+         Flush_Text (P);
+         P.Line_Pos := Last;
+         if not P.Context.Is_Hidden then
+            Wiki.Attributes.Clear (P.Attributes);
+            Wiki.Attributes.Append (P.Attributes, "src", Link);
+            P.Context.Filters.Add_Image (P.Document, Wiki.Strings.To_WString (Title),
+                                         P.Attributes);
+         end if;
+      end;
    end Parse_Image;
 
    --  Parse an external link:
@@ -136,19 +182,53 @@ package body Wiki.Parsers.Textile is
    --    "title":http-link
    procedure Parse_Link (P     : in out Parser;
                          Token : in Wiki.Strings.WChar) is
+      Pos  : Natural := P.Line_Pos;
+      Next : Natural := Wiki.Strings.Index (P.Line, Token, Pos);
+      C    : Wiki.Strings.WChar;
    begin
-      null;
+      if Next = 0 or Next = P.Line_Length then
+         Append (P.Text, Token);
+         return;
+      end if;
+
+      C := Wiki.Strings.Element(P.Line, Next + 1);
+      if C /= ':' then
+         Append (P.Text, Token);
+         return;
+      end if;
+
+      Flush_Text (P);
+      Wiki.Attributes.Clear (P.Attributes);
+
+      P.Empty_Line := False;
+      if not P.Context.Is_Hidden then
+         declare
+            Link : constant Strings.WString := Attributes.Get_Attribute (P.Attributes, HREF_ATTR);
+            Name : constant Strings.WString := Attributes.Get_Attribute (P.Attributes, NAME_ATTR);            
+         begin
+            P.Context.Filters.Add_Link (P.Document, Name, P.Attributes);
+         end;
+      end if;
    end Parse_Link;
 
-   --  Parse an italic or bold sequence or a list.
+   --  ------------------------------
+   --  Parse a bold sequence or a list.
    --  Example:
-   --    *name*         (italic)
-   --    **name**       (bold)
+   --    *name*         (bold)
    --    * item         (list)
+   --  ------------------------------
    procedure Parse_Bold_Or_List (P     : in out Parser;
                                  Token : in Wiki.Strings.WChar) is
+      C : Wiki.Strings.WChar;
    begin
-      null;
+      Peek (P, C);
+      if P.Empty_Line and C = ' ' then
+         Put_Back (P, C);
+         Common.Parse_List (P, Token);
+         return;
+      end if;
+      Put_Back (P, C);
+      Toggle_Format (P, BOLD);
    end Parse_Bold_Or_List;
 
    --  Parse a markdown table/column.
