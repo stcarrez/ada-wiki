cf050d65a87b9d7229d0c5fd8020aa82d56dbdb8
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-04-02 20:28:45 +0200

    Add the Newline procedure and update to indent the HTML output stream

--- src/wiki-streams-html-stream.adb
+++ src/wiki-streams-html-stream.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  wiki-streams-html-stream -- Generic Wiki HTML output stream
---  Copyright (C) 2016, 2020 Stephane Carrez
+--  Copyright (C) 2016, 2020, 2022 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +15,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+with Wiki.Helpers;
 package body Wiki.Streams.Html.Stream is
 
    --  Close the current XML entity if an entity was started
@@ -26,6 +26,19 @@ package body Wiki.Streams.Html.Stream is
                            Content : in String);
 
    --  ------------------------------
+   --  Write an optional newline or space.
+   --  ------------------------------
+   overriding
+   procedure Newline (Writer : in out Html_Output_Stream) is
+   begin
+      if Writer.Indent_Level > 0 and Writer.Text_Length > 0 then
+         Writer.Write (Wiki.Helpers.LF);
+         Writer.Empty_Line := True;
+      end if;
+      Writer.Text_Length := 0;
+   end Newline;
+
+   --  ------------------------------
    --  Close the current XML entity if an entity was started
    --  ------------------------------
    procedure Close_Current (Stream : in out Html_Output_Stream'Class) is
@@ -33,6 +46,7 @@ package body Wiki.Streams.Html.Stream is
       if Stream.Close_Start then
          Stream.Write ('>');
          Stream.Close_Start := False;
+         Stream.Empty_Line := False;
       end if;
    end Close_Current;
 
@@ -45,6 +59,10 @@ package body Wiki.Streams.Html.Stream is
       for I in Content'Range loop
          Stream.Write (Wiki.Strings.To_WChar (Content (I)));
       end loop;
+      if Content'Length > 0 then
+         Stream.Text_Length := Stream.Text_Length + Content'Length;
+         Stream.Empty_Line := False;
+      end if;
    end Write_String;
 
    --  ------------------------------
@@ -83,9 +101,20 @@ package body Wiki.Streams.Html.Stream is
                             Name   : in String) is
    begin
       Close_Current (Stream);
+      Stream.Indent_Pos := Stream.Indent_Pos + Stream.Indent_Level;
+      if Stream.Indent_Pos > 1 then
+         if not Stream.Empty_Line then
+            Stream.Write (Helpers.LF);
+         end if;
+         for I in 1 .. Stream.Indent_Pos loop
+            Stream.Write (' ');
+         end loop;
+      end if;
       Stream.Write ('<');
       Stream.Write_String (Name);
       Stream.Close_Start := True;
+      Stream.Text_Length := 0;
+      Stream.Empty_Line := False;
    end Start_Element;
 
    --  ------------------------------
@@ -95,15 +124,34 @@ package body Wiki.Streams.Html.Stream is
    procedure End_Element (Stream : in out Html_Output_Stream;
                           Name   : in String) is
    begin
+      if Stream.Indent_Pos > Stream.Indent_Level then
+         Stream.Indent_Pos := Stream.Indent_Pos - Stream.Indent_Level;
+      end if;
       if Stream.Close_Start then
          Stream.Write (" />");
          Stream.Close_Start := False;
       else
          Close_Current (Stream);
+         if Stream.Text_Length = 0 then
+            if not Stream.Empty_Line then
+               Stream.Write (Wiki.Helpers.LF);
+            end if;
+            if Stream.Indent_Pos > 1 then
+               Stream.Write (Helpers.LF);
+               for I in 1 .. Stream.Indent_Pos loop
+                  Stream.Write (' ');
+               end loop;
+            end if;
+         end if;
          Stream.Write ("</");
          Stream.Write_String (Name);
          Stream.Write ('>');
       end if;
+      if Stream.Indent_Level > 0 then
+         Stream.Write (Wiki.Helpers.LF);
+         Stream.Empty_Line := True;
+      end if;
+      Stream.Text_Length := 0;
    end End_Element;
 
    --  ------------------------------
@@ -115,6 +163,10 @@ package body Wiki.Streams.Html.Stream is
    begin
       Close_Current (Stream);
       Stream.Write_Escape (Content);
+      if Content'Length > 0 then
+         Stream.Text_Length := Stream.Text_Length + Content'Length;
+         Stream.Empty_Line := False;
+      end if;
    end Write_Wide_Text;
 
 end Wiki.Streams.Html.Stream;
