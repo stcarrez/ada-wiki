8faab3658961c96deb03425ed4caec72cc6e4ca6
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-05-06 11:50:55 +0200

    Refactor the MediaWiki parser to use the new wiki parser framework

--- src/wiki-parsers-mediawiki.adb
+++ src/wiki-parsers-mediawiki.adb
@@ -17,13 +17,16 @@
 -----------------------------------------------------------------------
 with Wiki.Nodes;
 with Wiki.Helpers;
+with Wiki.Parsers.Common;
 package body Wiki.Parsers.MediaWiki is
 
    use Wiki.Helpers;
-   use Wiki.Nodes;
+   use Wiki.Buffers;
+   use type Wiki.Nodes.Node_Kind;
 
-   Attr_Name              : constant String_Array (1 .. 1)
-     := (1 => NAME_ATTR'Access);
+   procedure Parse_Bold_Italic (Parser  : in out Parser_Type;
+                                Text    : in out Wiki.Buffers.Buffer_Access;
+                                From    : in out Positive);
 
    --  ------------------------------
    --  Parse an italic, bold or bold + italic sequence.
@@ -32,235 +35,171 @@ package body Wiki.Parsers.MediaWiki is
    --    '''name'''       (bold)
    --    '''''name'''''   (bold+italic)
    --  ------------------------------
-   procedure Parse_Bold_Italic (P     : in out Parser;
-                                Token : in Wiki.Strings.WChar) is
-      C     : Wiki.Strings.WChar;
-      Count : Natural := 1;
+   procedure Parse_Bold_Italic (Parser  : in out Parser_Type;
+                                Text    : in out Wiki.Buffers.Buffer_Access;
+                                From    : in out Positive) is
+      Count : Natural := Count_Occurence (Text, From, ''');
    begin
-      loop
-         Peek (P, C);
-         exit when C /= Token;
-         Count := Count + 1;
+      case Count is
+         when 1 =>
+            Common.Parse_Text (Parser, Text, From);
+            return;
+
+         when 2 =>
+            Toggle_Format (Parser, ITALIC);
+
+         when 3 =>
+            Toggle_Format (Parser, BOLD);
+
+         when 4 =>
+            Toggle_Format (Parser, BOLD);
+            Common.Parse_Text (Parser, Text, From);
+            Count := 3;
+
+         when 5 =>
+            Toggle_Format (Parser, BOLD);
+            Toggle_Format (Parser, ITALIC);
+
+         when others =>
+            Common.Parse_Text (Parser, Text, From);
+            return;
+
+      end case;
+      for I in 1 .. Count loop
+         Next (Text, From);
       end loop;
-      if Count > 10 then
-         Count := Count mod 10;
-         if Count = 0 then
-            Put_Back (P, C);
+   end Parse_Bold_Italic;
+
+   procedure Parse_Line (Parser : in out Parser_Type;
+                         Text   : in Wiki.Buffers.Buffer_Access) is
+      Pos    : Natural := 1;
+      C      : Wiki.Strings.WChar;
+      Buffer : Wiki.Buffers.Buffer_Access := Text;
+   begin
+      --  Feed the HTML parser if there are some pending state.
+      if not Wiki.Html_Parser.Is_Empty (Parser.Html) then
+         Common.Parse_Html_Element (Parser, Buffer, Pos, Start => False);
+         if Buffer = null then
             return;
          end if;
       end if;
 
-      case Count is
-         when 1 =>
-            Parse_Text (P, Token);
+      if Parser.Pre_Tag_Counter > 0 then
+         Common.Parse_Html_Preformatted (Parser, Buffer, Pos);
+         if Buffer = null then
+            return;
+         end if;
+      end if;
 
-         when 2 =>
-            Toggle_Format (P, ITALIC);
+      if Parser.Current_Node = Nodes.N_PREFORMAT then
+         if Buffer.Content (Pos) = ' ' then
+            Common.Append (Parser.Text, Buffer, Pos + 1);
+            return;
+         end if;
+         Pop_Block (Parser);
+      end if;
 
-         when 3 =>
-            Toggle_Format (P, BOLD);
+      if Parser.Current_Node = Nodes.N_HEADER then
+         Pop_Block (Parser);
+      end if;
 
-         when 4 =>
-            Toggle_Format (P, BOLD);
-            Parse_Text (P, Token);
+      C := Buffer.Content (Pos);
+      case C is
+         when CR | LF =>
+            Common.Parse_Paragraph (Parser, Buffer, Pos);
+            return;
 
-         when 5 =>
-            Toggle_Format (P, BOLD);
-            Toggle_Format (P, ITALIC);
+         when '=' =>
+            Common.Parse_Header (Parser, Buffer, Pos, '=');
+            if Buffer = null then
+               return;
+            end if;
+
+         when '-' =>
+            Common.Parse_Horizontal_Rule (Parser, Buffer, Pos, '-');
+            if Buffer = null then
+               return;
+            end if;
+
+         when '*' | '#' =>
+            if Common.Is_List (Buffer, Pos) then
+               Common.Parse_List (Parser, Buffer, Pos);
+            end if;
+
+         when ' ' =>
+            Parser.Preformat_Indent := 1;
+            Parser.Preformat_Fence := ' ';
+            Parser.Preformat_Fcount := 1;
+            Flush_Text (Parser, Trim => Right);
+            Pop_Block (Parser);
+            Push_Block (Parser, Nodes.N_PREFORMAT);
+            Common.Append (Parser.Text, Buffer, Pos + 1);
+            return;
+
+         when ';' =>
+            Common.Parse_Definition (Parser, Buffer, Pos);
+            return;
+
+         when ':' =>
+            if Parser.Current_Node = Nodes.N_DEFINITION then
+               Next (Buffer, Pos);
+               Common.Skip_Spaces (Buffer, Pos);
+            end if;
 
          when others =>
-            null;
-      end case;
-      Put_Back (P, C);
-   end Parse_Bold_Italic;
-
-   --  ------------------------------
-   --  Parse a list definition:
-   --    ;item 1
-   --    : definition 1
-   --  ------------------------------
-   procedure Parse_Definition (P     : in out Parser;
-                               Token : in Wiki.Strings.WChar) is
-   begin
-      if not P.Empty_Line then
-         Parse_Text (P, Token);
-         return;
-      end if;
-      Flush_Text (P);
-      Wiki.Attributes.Clear (P.Attributes);
-      if not P.Context.Is_Hidden then
-         P.Context.Filters.Push_Node (P.Document, Wiki.DD_TAG, P.Attributes);
-      end if;
-   end Parse_Definition;
-
-   --  ------------------------------
-   --  Parse a list definition:
-   --    ;item 1
-   --    : definition 1
-   --  ------------------------------
-   procedure Parse_Item (P     : in out Parser;
-                         Token : in Wiki.Strings.WChar) is
-   begin
-      if not P.Empty_Line then
-         Parse_Text (P, Token);
-         return;
-      end if;
-      Flush_Text (P);
-      Wiki.Attributes.Clear (P.Attributes);
-      if not P.Context.Is_Hidden then
-         if not P.In_List then
-            P.Context.Filters.Push_Node (P.Document, Wiki.DL_TAG, P.Attributes);
-         end if;
-         P.Context.Filters.Push_Node (P.Document, Wiki.DT_TAG, P.Attributes);
-      end if;
-      P.In_List := True;
-   end Parse_Item;
-
-   --  ------------------------------
-   --  Parse a horizontal rule.
-   --  Example:
-   --    ---- (dotclear)
-   --  ------------------------------
-   procedure Parse_Horizontal_Rule (P     : in out Parser;
-                                    Token : in Wiki.Strings.WChar) is
-      C     : Wiki.Strings.WChar;
-      Count : Natural := 1;
-   begin
-      loop
-         Peek (P, C);
-         exit when C /= Token;
-         Count := Count + 1;
-      end loop;
-      if Count >= 4 then
-         Flush_Text (P);
-         Flush_List (P);
-         if not P.Context.Is_Hidden then
-            P.Context.Filters.Add_Node (P.Document, Wiki.Nodes.N_HORIZONTAL_RULE);
-         end if;
-         if C /= LF and C /= CR then
-            Put_Back (P, C);
-         end if;
-      elsif P.Is_Dotclear and Count = 2 then
-         Toggle_Format (P, STRIKEOUT);
-         Put_Back (P, C);
-      else
-         for I in 1 .. Count loop
-            Append (P.Text, Token);
-         end loop;
-         Put_Back (P, C);
-      end if;
-   end Parse_Horizontal_Rule;
-
-   --  ------------------------------
-   --  Parse a template with parameters.
-   --  Example:
-   --    {{Name|param|...}}           MediaWiki
-   --    {{Name|param=value|...}}     MediaWiki
-   --    <<Name param=value ...>>     Creole
-   --    [{Name param=value ...}]     JSPWiki
-   --  ------------------------------
-   procedure Parse_Template (P     : in out Parser;
-                             Token : in Wiki.Strings.WChar) is
-      use type Wiki.Plugins.Wiki_Plugin_Access;
-
-      C      : Wiki.Strings.WChar;
-      Expect : Wiki.Strings.WChar;
-   begin
-      Peek (P, C);
-      if C /= Token then
-         Append (P.Text, Token);
-         Put_Back (P, C);
-         return;
-      end if;
-      Peek (P, C);
-      if C = Token then
-         Parse_Parameter (P, Token);
-         return;
-      end if;
-      Flush_Text (P);
-      Put_Back (P, C);
-      if Token = '{' then
-         Expect := '}';
-      else
-         Expect := '>';
-      end if;
-      Wiki.Attributes.Clear (P.Attributes);
-      if P.Context.Syntax = SYNTAX_MEDIA_WIKI then
-         Common.Parse_Parameters (P, '|', Expect, Attr_Name);
-      else
-         Common.Parse_Parameters (P, ' ', Expect, Attr_Name);
-      end if;
-      Peek (P, C);
-      if C /= Expect then
-         Put_Back (P, C);
-      end if;
-      P.Empty_Line := False;
-      declare
-         use type Wiki.Strings.UString;
-         Name    : constant Strings.WString := Attributes.Get_Attribute (P.Attributes, NAME_ATTR);
-         Plugin  : constant Wiki.Plugins.Wiki_Plugin_Access := P.Find (Name);
-         Context : Wiki.Plugins.Plugin_Context;
-         Ctx     : access Wiki.Plugins.Plugin_Context;
-      begin
-         if Plugin /= null then
-            --  Check that we are not including the template recursively.
-            Ctx := P.Context'Access;
-            while Ctx /= null loop
-               if Ctx.Ident = Name then
-                  Append (P.Text, "Recursive call to ");
-                  Append (P.Text, Name);
-                  return;
-               end if;
-               Ctx := Ctx.Previous;
-            end loop;
-            Context.Previous := P.Context'Unchecked_Access;
-            Context.Factory := P.Context.Factory;
-            Context.Syntax  := P.Context.Syntax;
-            Context.Variables := P.Attributes;
-            Context.Is_Included := True;
-            Context.Ident := Wiki.Strings.To_UString (Name);
-            Context.Filters.Set_Chain (P.Context.Filters);
-            Plugin.Expand (P.Document, P.Attributes, Context);
-         end if;
-      end;
-   end Parse_Template;
-
-   --  ------------------------------
-   --  Parse a template parameter and expand it.
-   --  Example:
-   --    {{{1}}}                      MediaWiki
-   --  ------------------------------
-   procedure Parse_Parameter (P     : in out Parser;
-                              Token : in Wiki.Strings.WChar) is
-      C      : Wiki.Strings.WChar;
-      Expect : Wiki.Strings.WChar;
-      Pos    : Wiki.Attributes.Cursor;
-   begin
-      if Token = '{' then
-         Expect := '}';
-      else
-         Expect := '>';
-      end if;
-      Common.Parse_Parameters (P, '|', Expect, Attr_Name);
-      Peek (P, C);
-      if C = Expect then
-         Peek (P, C);
-         if C /= Expect then
-            Put_Back (P, C);
-         end if;
-      else
-         Put_Back (P, C);
-      end if;
-      if not P.Context.Is_Hidden then
-         declare
-            Name : constant String := Attributes.Get_Value (Wiki.Attributes.First (P.Attributes));
-         begin
-            Pos := Wiki.Attributes.Find (P.Context.Variables, Name);
-            if Wiki.Attributes.Has_Element (Pos) then
-               Append (P.Text, Wiki.Attributes.Get_Wide_Value (Pos));
+            if Parser.Current_Node /= Nodes.N_PARAGRAPH then
+               Pop_List (Parser);
+               Push_Block (Parser, Nodes.N_PARAGRAPH);
             end if;
+
+      end case;
+
+      Main :
+      while Buffer /= null loop
+         declare
+            Last : Natural := Buffer.Last;
+         begin
+            while Pos <= Last loop
+               C := Buffer.Content (Pos);
+               case C is
+                  when ''' =>
+                     Parse_Bold_Italic (Parser, Buffer, Pos);
+                     exit Main when Buffer = null;
+
+                  when '/' =>
+                     Parse_Format_Double (Parser, Buffer, Pos, '/', Wiki.EMPHASIS);
+                     exit Main when Buffer = null;
+
+                  when '[' =>
+                     Common.Parse_Link (Parser, Buffer, Pos);
+                     exit Main when Buffer = null;
+
+                  when '{' =>
+                     Common.Parse_Template (Parser, Buffer, Pos, '{');
+                     exit Main when Buffer = null;
+
+                  when CR | LF =>
+                     Append (Parser.Text, ' ');
+                     Pos := Pos + 1;
+
+                  when '<' =>
+                     Common.Parse_Html_Element (Parser, Buffer, Pos, Start => True);
+                     exit Main when Buffer = null;
+
+                  when '&' =>
+                     Common.Parse_Entity (Parser, Buffer, Pos);
+                     exit Main when Buffer = null;
+
+                  when others =>
+                     Append (Parser.Text, C);
+                     Pos := Pos + 1;
+
+               end case;
+            end loop;
          end;
-      end if;
-   end Parse_Parameter;
+         Buffer := Buffer.Next_Block;
+         Pos := 1;
+      end loop Main;
+   end Parse_Line;
 
 end Wiki.Parsers.MediaWiki;
--- src/wiki-parsers-mediawiki.ads
+++ src/wiki-parsers-mediawiki.ads
@@ -15,73 +15,13 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-with Wiki.Parsers.Html;
-with Wiki.Parsers.Common;
 private package Wiki.Parsers.MediaWiki is
 
    pragma Preelaborate;
 
-   --  Parse an italic, bold or bold + italic sequence.
-   --  Example:
-   --    ''name''         (italic)
-   --    '''name'''       (bold)
-   --    '''''name'''''   (bold+italic)
-   --  ------------------------------
-   procedure Parse_Bold_Italic (P     : in out Parser;
-                                Token : in Wiki.Strings.WChar);
+   subtype Parser_Type is Parser;
 
-   --  Parse a list definition:
-   --    ;item 1
-   --    : definition 1
-   procedure Parse_Definition (P     : in out Parser;
-                               Token : in Wiki.Strings.WChar);
-
-   --  Parse a list definition:
-   --    ;item 1
-   --    : definition 1
-   procedure Parse_Item (P     : in out Parser;
-                         Token : in Wiki.Strings.WChar);
-
-   --  Parse a horizontal rule.
-   --  Example:
-   --    ----
-   procedure Parse_Horizontal_Rule (P     : in out Parser;
-                                    Token : in Wiki.Strings.WChar);
-
-   --  Parse a template parameter and expand it.
-   --  Example:
-   --    {{{1}}}                      MediaWiki
-   procedure Parse_Parameter (P     : in out Parser;
-                              Token : in Wiki.Strings.WChar);
-
-   --  Parse a template with parameters.
-   --  Example:
-   --    {{Name|param|...}}           MediaWiki
-   --    {{Name|param=value|...}}     MediaWiki
-   --    <<Name param=value ...>>     Creole
-   --    [{Name param=value ...}]     JSPWiki
-   procedure Parse_Template (P     : in out Parser;
-                             Token : in Wiki.Strings.WChar);
-
-   Wiki_Table : aliased constant Parser_Table
-     := (
-         16#0A# => Parse_End_Line'Access,
-         16#0D# => Parse_End_Line'Access,
-         Character'Pos (' ') => Parse_Space'Access,
-         Character'Pos ('=') => Common.Parse_Header'Access,
-         Character'Pos (''') => Parse_Bold_Italic'Access,
-         Character'Pos ('[') => Common.Parse_Link'Access,
-         Character'Pos ('\') => Common.Parse_Line_Break'Access,
-         Character'Pos ('#') => Common.Parse_List'Access,
-         Character'Pos ('*') => Common.Parse_List'Access,
-         Character'Pos ('<') => Parse_Maybe_Html'Access,
-         Character'Pos ('&') => Html.Parse_Entity'Access,
-         Character'Pos ('-') => Parse_Horizontal_Rule'Access,
-         Character'Pos (';') => Parse_Item'Access,
-         Character'Pos ('{') => Parse_Template'Access,
-         Character'Pos (':') => Parse_Definition'Access,
-         Character'Pos ('~') => Common.Parse_Escape'Access,
-         others => Parse_Text'Access
-        );
+   procedure Parse_Line (Parser : in out Parser_Type;
+                         Text   : in Wiki.Buffers.Buffer_Access);
 
 end Wiki.Parsers.MediaWiki;
